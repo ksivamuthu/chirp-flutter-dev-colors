1cc5ff9f05467c0c523e93d3ce8b5c3032ba5c89
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2019-06-17 19:50:56 -0400

    Update tests

---
+++ chirp_flutter_light/test/main_test.dart
@@ -0,0 +1,23 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:chirp_flutter_light/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Angular'), findsOneWidget);
+    expect(find.text('React'), findsOneWidget);
+    expect(find.text('Vue'), findsOneWidget);
+  });
+}
