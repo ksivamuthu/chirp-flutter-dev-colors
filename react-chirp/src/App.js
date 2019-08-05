0805744e964af39de873ea848962397e4c94e22b
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2019-08-05 13:07:27 -0400

    React chirp

--- react-chirp/src/App.js
+++ react-chirp/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import GoogleFontLoader from 'react-google-font-loader';
-import { ListItem, List, ListItemText, Divider, ListItemIcon } from '@material-ui/core';
+import { IconButton, ListItem, List, ListItemText, Divider, ListItemIcon, TextField, Button, Grid, AppBar, Toolbar, Typography, MenuItem, Icon } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { faLightbulb, faSync } from '@fortawesome/free-solid-svg-icons'
 import ChirpConnect from 'chirp-js-sdk'
 
 const chirp = new ChirpConnect('43790CCAde71e7bD92bb74bEb')
-chirp.setConfig('ultrasonic-multichannel');
+chirp.setConfig('standard');
 
 const primaryText = {
   fontSize: '24px',
@@ -20,12 +20,14 @@ const secondaryText = {
 };
 
 class App extends Component {
-
+  _username = '';
   constructor() {
     super();
-    this.state = { data: null };
+    this.state = { data: null, username: '' };
   }
   componentDidMount() {
+    this._username = window.localStorage['username'] || '';
+    this.setState({ username: this._username });
     fetch('/api/devcolors').then(response => response.json())
       .then(data => this.setState({ data: data }));
   }
@@ -33,6 +35,20 @@ class App extends Component {
   handleClick(item) {
     const payload = new TextEncoder('utf-8').encode(item.color)
     chirp.send(payload)
+  }
+
+  onSaveClicked() {
+    this.setState({ username: this._username });
+    window.localStorage['username'] = this._username;
+  }
+
+  handleChange(event) {
+    this._username = event.target.value;
+  }
+
+  resetData() {
+    window.localStorage['username'] = '';
+    this.setState({ username: ''});
   }
 
   render() {
@@ -46,29 +62,67 @@ class App extends Component {
             }
           ]}
         />
-        <div className="App">
-          <h1>Chirping Devices</h1>
-          <List className="ColorsList">
-            {this.state.data && this.state.data.length > 0 && <div>
-              {this.state.data.map((item) => {
-                return (
-                  <Fragment key={item.title}>
-                    <ListItem onClick={() => this.handleClick(item)} button={true} alignItems="flex-start">
-                      <ListItemIcon>
-                        <FontAwesomeIcon style={{ "color": item.color }} className="bulb" icon={faLightbulb} />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item.title}
-                        primaryTypographyProps={{ style: primaryText }}
-                        secondary={item.subtitle}
-                        secondaryTypographyProps={{ style: secondaryText }} />
-                    </ListItem>
-                    <Divider variant="fullWidth" component="li" />
-                  </Fragment>
-                )
-              })}
-            </div>}
-          </List>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h5" style={{ flexGrow: 1 }}>
+                Chirping Devices
+               </Typography>
+              <IconButton onClick={this.resetData.bind(this)}>
+                <FontAwesomeIcon icon={faSync} color={"white"} />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <div className="App">
+            {!this.state.username && <Grid container spacing={0}
+              direction="column"
+              alignItems="center"
+              justify="center">
+              <TextField
+                style={{ width: "100%" }}
+                required
+                id="outlined-required"
+                label="Please enter your name" s
+                placeholder="Name"
+                margin="normal"
+                onChange={this.handleChange.bind(this)}
+                variant="outlined"
+              />
+              <Grid item>
+                <Button onClick={this.onSaveClicked.bind(this)} style={{ width: "120px" }} variant="contained" color="primary">
+                  Save
+              </Button>
+              </Grid>
+            </Grid>}
+            {this.state.username && <Fragment>
+              <Typography variant="h4">
+                Welcome {this.state.username}
+              </Typography>
+              <List className="ColorsList">
+                {this.state.data && this.state.data.length > 0 && <div>
+                  {this.state.data.map((item) => {
+                    return (
+                      <Fragment key={item.title}>
+                        <ListItem onClick={() => this.handleClick(item)} button={true} alignItems="flex-start">
+                          <ListItemIcon>
+                            <FontAwesomeIcon style={{ "color": item.color }} className="bulb" icon={faLightbulb} />
+                          </ListItemIcon>
+                          <ListItemText
+                            primary={item.title}
+                            primaryTypographyProps={{ style: primaryText }}
+                            secondary={item.subtitle}
+                            secondaryTypographyProps={{ style: secondaryText }} />
+                        </ListItem>
+                        <Divider variant="fullWidth" component="li" />
+                      </Fragment>
+                    )
+                  })}
+                </div>
+                }
+              </List>
+            </Fragment>
+            }
+          </div>
         </div>
       </Fragment>
     );
